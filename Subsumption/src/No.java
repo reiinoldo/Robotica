ecaaefa4cf0d0b163a50bfda5ce0902eb234c8ee
Eli <eli.t.desouza@gmail.com>
2015-05-12 21:47:00 -0300

    Começando....

---
+++ Subsumption/src/No.java
@@ -0,0 +1,25 @@
+
+public class No {
+	private No frente;
+	private No direita;
+	private No esquerda;
+	
+	public No getFrente() {
+		return frente;
+	}
+	public void setFrente(No frente) {
+		this.frente = frente;
+	}
+	public No getDireita() {
+		return direita;
+	}
+	public void setDireita(No direita) {
+		this.direita = direita;
+	}
+	public No getEsquerda() {
+		return esquerda;
+	}
+	public void setEsquerda(No esquerda) {
+		this.esquerda = esquerda;
+	}
+}
--- Subsumption/src/Visao.java
+++ Subsumption/src/Visao.java
@@ -5,34 +5,50 @@ import lejos.robotics.subsumption.Behavior;
 
 
 public class Visao implements Behavior {
+		
+	public static final int DIREITA = 1;
+	public static final int ESQUERDA = 2;
+	public static final int FRENTE = 3;
 	
-	
-	static final int DIREITA = 1;
-	static final int ESQUERDA = 2;
-	static final int FRENTE = 3;
+	private No raiz;
+	private No noAtual;
 	
 	UltrasonicSensor sensor;
 	public Visao(UltrasonicSensor sensor){
 		this.sensor = sensor;
+		this.raiz = new No();
+		noAtual = raiz;
 	}
 	
-	public int retornaDirecao(){
+	public int retornaDirecao(No no){
 		int retorno = 0;
 			
-		if (sensor.getDistance() > 30 && retorno == 0)
+		if (sensor.getDistance() > 30 /*&& retorno == 0*/){
 			retorno = FRENTE;
-		
+			if(no != null){
+				no.setFrente(new No());
+			}
+		}
+					
 		// direita
 		Motor.C.rotate(90);		
-		if (sensor.getDistance() > 30 && retorno == 0)
+		if (sensor.getDistance() > 30 /*&& retorno == 0*/){
 			retorno = DIREITA;
+			
+			if(no != null){
+				no.setDireita(new No());
+			}
+		}
 		
 		// esquerda				
 		Motor.C.rotate(-180);
-		if (sensor.getDistance() > 30 && retorno == 0)
+		if (sensor.getDistance() > 30 /*&& retorno == 0*/){
 			retorno = ESQUERDA;
-		
-		// fazer backtrack
+			
+			if(no != null){
+				no.setEsquerda(new No());
+			}
+		}			
 		
 		// frente
 		Motor.C.rotate(90);	
@@ -48,7 +64,7 @@ public class Visao implements Behavior {
 	@Override
 	public void action() {
 		// TODO Auto-generated method stub
-		switch (retornaDirecao()) {
+		switch (retornaDirecao(noAtual)) {
 		case FRENTE:
 			Motor.A.rotate(500, true);
 			Motor.B.rotate(500);
@@ -62,6 +78,9 @@ public class Visao implements Behavior {
 		case ESQUERDA:
 			Motor.A.rotate(-210, true);
 			Motor.B.rotate(210);
+			break;
+			//BackTracking
+		case 0:
 			break;
 		default:
 			break;
